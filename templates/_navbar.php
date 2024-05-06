3caf8e8d76fd965d6c19f894836458085fa19b86
Philip-V-W <philip.vonwilde@icloud.com>
2024-05-07 00:09:40 +0200

    navbar changes, modified styles and fixed render_functions.php

--- templates/_navbar.php
+++ templates/_navbar.php
@@ -4,25 +4,23 @@
     </a>
 </div>
 
-<nav class="navbar navbar-expand-lg custom_background w-100">
+<nav class="navbar navbar-expand-lg w-100">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
-                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<!--        <button class="navbar-toggler btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"-->
+<!--                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--            <span class="navbar-toggler-icon"></span>-->
+<!--        </button>-->
         <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../toys.php">Tous les jeux</a>
+                    <a class="nav-link btn btn-primary text-light" aria-current="page" href="#">Tous les jeux</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                       aria-expanded="false">
-                        Par console
-                    </a>
-<!--                    <ul class="dropdown-menu">-->
-<!--                        --><?php //get_brands_with_count(); ?>
-<!--                    </ul>-->
+                    <a class="nav-link dropdown-toggle btn btn-primary text-light" href="#" role="button" data-bs-toggle="dropdown"
+                       aria-expanded="false">Par console </a>
+                    <ul class="dropdown-menu">
+<!--                        --><?php //get_games_by_platform(); ?>
+                    </ul>
                 </li>
             </ul>
         </div>
--- templates/render_functions.php
+++ templates/render_functions.php
@@ -3,12 +3,13 @@
 // methode pour render tous les jeux video
 function render_all_video_games($jeu)
 { ?>
-    <a class="card m-2" href="../detail.php?jeu_id=<?php echo $jeu['id'] ?>" style="width: 18rem;">
-        <div class="d-flex flex-column align-items-center card-body">
+    <div class="card m-2">
+        <div class="d-flex flex-column align-items-center">
             <img class="jeu_image" src="../images/games/<?php echo $jeu['image_path'] ?>"
                  alt="image de <?php echo $jeu['titre'] ?>">
             <h5 class="jeu_nom"><?php echo $jeu['titre'] ?></h5>
-            <p class="jeu_prix"><?php echo str_replace('.', ',', $jeu['prix']) ?>€</p>
+            <p class="jeu_prix"><?php echo number_format($jeu['prix'] / 100, 2, ',', ' '); ?>€</p>
+            <a class="align-self-start btn btn-primary ms-3 mb-3 text-light" href="../detail.php?jeu_id=<?php echo $jeu['id'] ?>">Voir détail</a>
         </div>
-    </a>
+    </div>
 <?php }
