647814c847d985bd75126ed2e5ce6c6013bb9942
Philip-V-W <philip.vonwilde@icloud.com>
2024-05-12 23:19:46 +0200

    par prix, par age and par both avis filters finished, pagination started but not finished.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <?php require_once('./templates/_navbar.php') ?>
 
 <?php
-
 // on recupere le numero de la page
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
 // on definit le nombre de jeux par page
@@ -15,21 +14,27 @@ $records_per_page = 4;
 $total_records = get_total_records();
 // on calcule le nombre de pages
 $total_pages = ceil($total_records / $records_per_page);
-
 ?>
+
     <div class="row justify-content-center">
         <?php get_all_video_games($page, $records_per_page) ?>
     </div>
-<?php
 
+<?php
 // on affiche les liens pour naviguer entre les pages
+echo '<nav aria-label="page navigation">';
+echo '<ul class="pagination justify-content-center my-5">';
+
 for ($i = 1; $i <= $total_pages; $i++) {
     if ($i == $page) {
-        echo "$i ";
+        echo '<li class="page-item active"><a class="page-link" href="#">' . $i . '</a></li>';
     } else {
-        echo "<a href=\"index.php?page=$i\">$i</a> ";
+        echo '<li class="page-item"><a class="page-link" href="index.php?page=' . $i . '">' . $i . '</a></li>';
     }
 }
+
+echo '</ul>';
+echo '</nav>';
 ?>
 
 <?php require_once('./templates/_footer.php') ?>
